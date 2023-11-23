7e9ff7c0c28a6cacd8cfd7dbff080aab5982763f
ASalasAbaunza <alex.aba2409@gmail.com>
2023-11-22 21:42:40 -0600

    [Modelos creados y DB conectada]

---
+++ server/src/models/Activity.js
@@ -0,0 +1,31 @@
+const DataTypes = require('sequelize');
+
+module.exports = (sequelize) => {
+  sequelize.define('Activity', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoincrement: true,
+        primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    difficulty: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+            min: 1,
+            max: 5,
+        },
+    },
+    duration: {
+        type: DataTypes.DECIMAL,
+        allowNull: true,
+    },
+    season: {
+        type: DataTypes.ENUM('Summer', 'Fall', 'Winter', 'Spring'),
+        allowNull: false,
+    },
+  }, { timestamps: false });
+};
--- server/src/models/Country.js
+++ server/src/models/Country.js
@@ -1,12 +1,38 @@
-const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+const DataTypes = require('sequelize');
+
 module.exports = (sequelize) => {
-  // defino el modelo
   sequelize.define('Country', {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    flag: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    continent: {
+      type: DataTypes.ENUM('North America', 'South America', 'Europe', 'Asia', 'Africa', 'Oceania'),
+      allowNull: false,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subregion: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    area: {
+      type: DataTypes.DECIMAL,
+      allowNull: true,
+    },
+    population: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }, { timestamps: false });
 };
